4ecbab7414a16ef1236d40ae6f120bef57e9d96f
zhangbc <zhangbocheng189@163.com>
2019-11-23 00:02:33 +0800

    [add] add spring boot 2.1.10.

---
+++ src/main/java/com/team/bromall/BromallApplication.java
@@ -0,0 +1,19 @@
+package com.team.bromall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 应用程序入口
+ * @author zhangbocheng
+ * @date 2019-11-23
+ */
+@SpringBootApplication
+public class BromallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BromallApplication.class, args);
+	}
+
+}
